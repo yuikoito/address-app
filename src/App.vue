140e01f158d8f1f5d5bcb7f9a6b7fdbc8d78d4cb
YuikoIto <toffywing@gmail.com>
2020-07-15 21:34:24 +0900

    add sidenav

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <SideNav/>
+    <v-content>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SideNav from './components/SideNav'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'App',
+  components: {
+    SideNav
+  },
+  data () {
+    return {
+      //
     }
   }
 }
-</style>
+</script>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-layout wrap style="height: 200px;">
+
+    <v-container>
+      <v-layout justify-center>
+        <v-btn color="pink" dark @click.stop="drawer = !drawer">Toggle</v-btn>
+      </v-layout>
+    </v-container>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://pbs.twimg.com/profile_images/1267130660860223489/AS1PLP_-_400x400.jpg">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>Waniko Wani</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile v-for="item in items" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      items: [
+        { title: '連絡先一覧', icon: 'list' }
+      ]
+    }
+  }
+}
+</script>
